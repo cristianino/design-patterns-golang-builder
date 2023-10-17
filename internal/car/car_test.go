d807c57e044bcd63a1103db09289900ba8d55e14
Cristian Niño <cristian.nino@rocketfy.co>
2023-10-17 17:52:38 -0500

    chore: add test to buggy builder

--- internal/car/car_test.go
+++ internal/car/car_test.go
@@ -22,3 +22,19 @@ func TestSportsCarBuilder(t *testing.T) {
 	assert.Equal(t, "Ruedas deportivas", sportsCar.Wheels)
 	assert.Equal(t, "Chasis deportivo", sportsCar.Chassis)
 }
+
+func TestBuggyCarBuilder(t *testing.T) {
+	// Crear un constructor de carro deportivo
+	builderBuggy := &builder.BuggiesCarBuilder{}
+
+	// Crear un director con el constructor
+	director := builder.NewCarBuildDirector(builderBuggy)
+
+	// Construir el carro
+	buggyCar := director.Construct()
+
+	// Verificar que el carro deportivo se construyó correctamente
+	assert.Equal(t, "Motor de buggy", buggyCar.Engine)
+	assert.Equal(t, "Ruedas de buggy", buggyCar.Wheels)
+	assert.Equal(t, "Chasis de buggy", buggyCar.Chassis)
+}
